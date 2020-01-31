be5bcda9cc9c5d2708e8a81535afa6eaebdf7229
Maarsupial <30292497+Maarsupial@users.noreply.github.com>
2020-01-31 09:56:13 -0300

    Create ODBCManager.cs

---
+++ ODBCManager.cs
@@ -0,0 +1,153 @@
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+
+namespace ODBC_Connection
+{
+    ///<summary>
+    /// Class to assist with creation and removal of ODBC DSN entries
+    ///</summary>
+    public static class ODBCManager
+    {
+        private const string ODBC_INI_REG_PATH = @"SOFTWARE\ODBC\ODBC.INI\";
+        private const string ODBCINST_INI_REG_PATH = @"SOFTWARE\ODBC\ODBCINST.INI\";
+        private const string ODBC32_INI_REG_PATH = @"SOFTWARE\WOW6432Node\ODBC\ODBC.INI\";
+        private const string ODBCINST32_INI_REG_PATH = @"SOFTWARE\WOW6432Node\ODBC\ODBCINST.INI\";
+
+        /// <summary>
+        /// Creates a new DSN entry with the specified values. If the DSN exists, the values are updated.
+        /// </summary>
+        /// <param name="dsnName">Name of the DSN for use by client applications</param>
+        /// <param name="description">Description of the DSN that appears in the ODBC control panel applet</param>
+        /// <param name="server">Network name or IP address of database server</param>
+        /// <param name="driverName">Name of the driver to use</param>
+        /// <param name="trustedConnection">True to use NT authentication, false to require applications to supply username/password in the connection string</param>
+        /// <param name="database">Name of the datbase to connect to</param>
+        public static void CreateDSN64(string dsnName, string description, string server, string driverName, bool trustedConnection, string database)
+        {
+            // Lookup driver path from driver name
+            var driverKey = Registry.LocalMachine.CreateSubKey(ODBCINST_INI_REG_PATH + driverName);
+            if (driverKey == null) throw new Exception(string.Format("ODBC Registry key for driver '{0}' does not exist", driverName));
+            string driverPath = driverKey.GetValue("Driver").ToString();
+
+            // Add value to odbc data sources
+            var datasourcesKey = Registry.LocalMachine.CreateSubKey(ODBC_INI_REG_PATH + "ODBC Data Sources");
+            if (datasourcesKey == null) throw new Exception("ODBC Registry key for datasources does not exist");
+            datasourcesKey.SetValue(dsnName, driverName);
+
+            // Create new key in odbc.ini with dsn name and add values
+            var dsnKey = Registry.LocalMachine.CreateSubKey(ODBC_INI_REG_PATH + dsnName);
+            if (dsnKey == null) throw new Exception("ODBC Registry key for DSN was not created");
+            dsnKey.SetValue("Database", database);
+            dsnKey.SetValue("Description", description);
+            dsnKey.SetValue("Driver", driverPath);
+            dsnKey.SetValue("LastUser", Environment.UserName);
+            dsnKey.SetValue("Server", server);
+            dsnKey.SetValue("Database", database);
+            dsnKey.SetValue("Trusted_Connection", trustedConnection ? "Yes" : "No");
+        }
+
+        public static void CreateDSN32(string dsnName, string description, string server, string driverName, bool trustedConnection, string database)
+        {
+            // Lookup driver path from driver name
+            var driverKey = Registry.LocalMachine.CreateSubKey(ODBCINST32_INI_REG_PATH + driverName);
+            if (driverKey == null) throw new Exception(string.Format("ODBC Registry key for driver '{0}' does not exist", driverName));
+            string driverPath = driverKey.GetValue("Driver").ToString();
+
+            // Add value to odbc data sources
+            var datasourcesKey = Registry.LocalMachine.CreateSubKey(ODBC32_INI_REG_PATH + "ODBC Data Sources");
+            if (datasourcesKey == null) throw new Exception("ODBC Registry key for datasources does not exist");
+            datasourcesKey.SetValue(dsnName, driverName);
+
+            // Create new key in odbc.ini with dsn name and add values
+            var dsnKey = Registry.LocalMachine.CreateSubKey(ODBC32_INI_REG_PATH + dsnName);
+            if (dsnKey == null) throw new Exception("ODBC Registry key for DSN was not created");
+            dsnKey.SetValue("Database", database);
+            dsnKey.SetValue("Description", description);
+            dsnKey.SetValue("Driver", driverPath);
+            dsnKey.SetValue("LastUser", Environment.UserName);
+            dsnKey.SetValue("Server", server);
+            dsnKey.SetValue("Database", database);
+            dsnKey.SetValue("Trusted_Connection", trustedConnection ? "Yes" : "No");
+        }
+
+        /// <summary>
+        /// Removes a DSN entry
+        /// </summary>
+        /// <param name="dsnName">Name of the DSN to remove.</param>
+        public static void RemoveDSN(string dsnName)
+        {
+            // Remove DSN key
+            Registry.LocalMachine.DeleteSubKeyTree(ODBC_INI_REG_PATH + dsnName);
+
+            // Remove DSN name from values list in ODBC Data Sources key
+            var datasourcesKey = Registry.LocalMachine.CreateSubKey(ODBC_INI_REG_PATH + "ODBC Data Sources");
+            if (datasourcesKey == null) throw new Exception("ODBC Registry key for datasources does not exist");
+            datasourcesKey.DeleteValue(dsnName);
+        }
+
+        ///<summary>
+        /// Checks the registry to see if a DSN exists with the specified name
+        ///</summary>
+        ///<param name="dsnName"></param>
+        ///<returns></returns>
+        public static bool DSNExists(string dsnName)
+        {
+            var driversKey = Registry.LocalMachine.CreateSubKey(ODBCINST_INI_REG_PATH + "ODBC Drivers");
+            if (driversKey == null) throw new Exception("ODBC Registry key for drivers does not exist");
+
+            return driversKey.GetValue(dsnName) != null;
+        }
+
+        ///<summary>
+        /// Returns an array of driver names installed on the system
+        ///</summary>
+        ///<returns></returns>
+        public static string[] GetInstalledDrivers()
+        {
+            var driversKey = Registry.LocalMachine.CreateSubKey(ODBCINST_INI_REG_PATH + "ODBC Drivers");
+            if (driversKey == null) throw new Exception("ODBC Registry key for drivers does not exist");
+
+            var driverNames = driversKey.GetValueNames();
+
+            var ret = new List<string>();
+
+            foreach (var driverName in driverNames)
+            {
+                if (driverName != "(Default)")
+                {
+                    ret.Add(driverName);
+                }
+            }
+
+            return ret.ToArray();
+        }
+
+        ///<summary>
+        /// Opens an ODBC connection with an specified DSN and his arguments
+        ///</summary>
+        /// <param name="dsn">Name of the DSN for use by client applications.</param>
+        /// <param name="driver">Name of the driver to use.</param>
+        /// <param name="server">Network name or IP address of database server.</param>
+        /// <param name="uid">Username to be used in the connection.</param>
+        /// <param name="pwd">Password to be used in the connection.</param>
+        public static void ConnectODBC(string dsn, string driver, string server, string uid, string pwd)
+        {
+            OdbcConnectionStringBuilder builder = new OdbcConnectionStringBuilder();
+            builder.Dsn = dsn;
+
+            // Call the Add method to explicitly add key/value
+            // pairs to the internal collection.
+            builder.Add("Driver", driver);
+            builder.Add("Server", server);
+            builder.Add("Uid", uid);
+            builder.Add("Pwd", pwd);
+
+            OdbcConnection connection = new OdbcConnection(builder.ConnectionString);
+            connection.Open();
+        }
+    }
+}
